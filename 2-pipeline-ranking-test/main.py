3af5d3e553875f3c2c08604a38b752504ea6c637
João Vítor da Costa Ferreira <up202208393@up.pt>
2025-11-27 14:58:19 +0000

    [ADD] Beginning of k-folt testing

--- 2-pipeline-ranking-test/main.py
+++ 2-pipeline-ranking-test/main.py
@@ -5,7 +5,7 @@ from teamRanks import TeamRanks
 from teamStats import TeamStats
 
 def player_stats(input_df, test_years):
-    if not os.path.exists("outputs/predicted_players_stats.csv"):
+    # if not os.path.exists("outputs/predicted_players_stats.csv"):
         training_df = pd.read_csv("../database/final/players_teams.csv")
         training_df = training_df[~(training_df["year"].isin(test_years))]
         training_df = training_df.reset_index(drop=True)
@@ -15,12 +15,12 @@ def player_stats(input_df, test_years):
         ps_input = ps_model.preprocessInput(input_df)
         print("PlayerStats results generated!")
         return ps_model.generateResult(ps_input)
-    else:
-        print("[CACHED] PlayerStats")
-        return pd.read_csv("outputs/predicted_players_stats.csv")
+    # else:
+    #     print("[CACHED] PlayerStats")
+    #     return pd.read_csv("outputs/predicted_players_stats.csv")
         
 def team_stats(input_df, test_years):
-    if not os.path.exists("outputs/predicted_team_stats.csv"):
+    # if not os.path.exists("outputs/predicted_team_stats.csv"):
         training_df = pd.read_csv("../database/final/teams.csv")
         training_df = training_df[~training_df["year"].isin(test_years)]
         ts_model = TeamStats(training_df)
@@ -30,9 +30,9 @@ def team_stats(input_df, test_years):
         ts_input = TeamStats.filterFeatures(ts_input)
         print("TeamStats results generated!")
         return ts_model.generateResults(ts_input)
-    else:
-        print("[CACHED] TeamStats")
-        return pd.read_csv("outputs/predicted_team_stats.csv")
+    # else:
+    #     print("[CACHED] TeamStats")
+    #     return pd.read_csv("outputs/predicted_team_stats.csv")
         
 def team_ranks(input_df, test_years):
     training_df = pd.read_csv("../database/final/teams.csv")
@@ -43,22 +43,33 @@ def team_ranks(input_df, test_years):
     tr_input = tr_model.preprocessInput(input_df)
     print("TeamStats results generated!")
     return tr_model.generateResults(tr_input)
-
-def main():
-    # Simulate the input file
-    test_years = [10]
-    input_df = pd.read_csv("../database/final/players_teams.csv")
-    input_df = input_df[["playerID", "year", "tmID", "stint"]]
-    input_df = input_df[input_df["year"].isin(test_years)]
-    input_df = input_df.reset_index(drop=True)
-    print("Input loaded!")
-
+    
+def generate_results(input_df, test_years):
     ps_output = player_stats(input_df, test_years)      # Step 1: Generate player stats from teammates
     ts_output = team_stats(ps_output, test_years)       # Step 2: Generate team stats from player stats
     tr_output = team_ranks(ts_output, test_years)       # Step 3: Generate team ranks from team stats
+    return tr_output
 
-    print(tr_output)
+def test(input_df):
+    output = pd.DataFrame()
+    for year in range(1, 11):
+        print("Year: ", year)
+        
+        # Simulate the input file
+        test_years = [year]
+        this_input_df = input_df[["playerID", "year", "tmID", "stint"]]
+        this_input_df = this_input_df[this_input_df["year"].isin(test_years)]
+        this_input_df = this_input_df.reset_index(drop=True)
+        print("Input loaded!")
+        
+        this_year_output = generate_results(this_input_df, test_years)
+        output = pd.concat([output, this_year_output], ignore_index=True)
 
+    print(output)
+
+def main():
+    input_df = pd.read_csv("../database/final/players_teams.csv")
+    test(input_df)
 
 if __name__ == "__main__":
     main()

68051043df2a562fda1624bf6ec01576549b310f
João Vítor da Costa Ferreira <up202208393@up.pt>
2025-11-29 16:25:35 +0000

    [ADD] Rename "Kim Perrot Sportsmanship" to "Kim Perrot Sportsmanship Award" in awards_players.csv

--- database/cleaner.py
+++ database/cleaner.py
@@ -32,9 +32,24 @@ columns_to_remove_redundancy = {
     ],
 }
 
+values_to_rename = {
+    "awards_players": {
+        "award": {
+            "Kim Perrot Sportsmanship": "Kim Perrot Sportsmanship Award"
+        }
+    }
+}
+
 def remove_columns(df, remove, csv_name):
     cols = remove.get(csv_name, [])
     df = df.drop(columns=[col for col in cols if col in df.columns])
+    return df
+    
+def rename_values(df, rename, csv_name):
+    if csv_name in rename:
+        for col, mapping in rename[csv_name].items():
+            if col in df.columns:
+                df[col] = df[col].replace(mapping)
     return df
 
 def save_version(df, output_path, csv_name):
@@ -63,6 +78,9 @@ def clean_csv(name):
 
     # Drop redundant columns
     df = remove_columns(df, columns_to_remove_redundancy, name)
+    
+    # Rename "wrong" values
+    df = rename_values(df, values_to_rename, name)
 
     # Save final version
     save_version(df, FINAL_FOLDER, name)
